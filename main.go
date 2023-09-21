5d0faa1505c9a98341cc871c1a99d78da40bf879
ashwini700 <122772415+ashwini700@users.noreply.github.com>
2023-09-21 14:51:40 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference"
+
+	fmt.Println("Welcome to", conferenceName, "Booking Application")
+	fmt.Println("Get your tickets here to attend")
+
+}
